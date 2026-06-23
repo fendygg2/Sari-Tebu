f8bf9cf4d2e0743826fe412b30bc346c4d14c919
Aelberth Cheong <aelberth.cheong@outlook.com>
2026-06-23 07:21:11 +0700

    fix: many things

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -10,7 +10,7 @@ echo "$(date --utc +%FT%TZ): Releasing new server version. $BUILD_VERSION"
 echo "$(date --utc +%FT%TZ): Running Build..."
 docker compose rm -f
 docker compose build
-OLD_CONTAINER=$(docker ps -aqf "name=sari-tebu-server")
+OLD_CONTAINER=$(docker ps -aqf "name=server")
 echo "$(date --utc +%FT%TZ): Scaling server up..."
 BUILD_VERSION=$BUILD_VERSION docker compose up -d --no-deps --scale server=2 --no-recreate server
 sleep 30
@@ -18,5 +18,5 @@ echo "$(date --utc +%FT%TZ): Scaling old server down..."
 docker container rm -f $OLD_CONTAINER
 docker compose up -d --no-deps --scale server=1 --no-recreate server
 echo "$(date --utc +%FT%TZ): Reloading caddy..."
-CADDY_CONTAINER=$(docker ps -aqf "name=sari-tebu-caddy")
+CADDY_CONTAINER=$(docker ps -aqf "name=caddy")
 docker exec $CADDY_CONTAINER caddy reload -c /etc/caddy/Caddyfile
